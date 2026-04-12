ed06976266d589f6e46e8fc88dc1b5d6feebe78b
Sean-VM <sicsicrr11@gmail.com>
2026-04-12 15:00:22 +0300

    part2 progress

--- encoder.c
+++ encoder.c
@@ -5,18 +5,33 @@ FILE* output;
 FILE* input;
 FILE* errors;
 unsigned char password[] = "1234";
+char** encstr; //encoder string
+int add,subtract;
+int shiftA=0;
 
-int encode(int c){
+
+int encode(int c,int index){
+    if(add){
+        int addition = encstr[index]-shiftA;
+        c = c+addition;
+    }
+    if(subtract){
+        int subtraction = encstr[index]-shiftA;
+        c = c - subtraction;
+    }
     return c;
 }
 
 
 int main(int argc,char **argv){
+    add=0;
+    subtract=0;
     output=stdout;
     input=stdin;
     errors=stderr;
     int debug=1; //default is on
     for(int i=0;i<argc;i++){
+        //part1-debug
         if(debug){
             fprintf(errors,"%s \n",argv[i]);
         }
@@ -28,12 +43,29 @@ int main(int argc,char **argv){
         if(strncmp(argv[i],"+D",2)==0 && strcmp(argv[i]+2,(char*)password)==0){
             debug=1;
         }
-    }
+        //end of part1-debug
 
-    //second part
+        //part2-encode
+        if(strncmp(argv[i],"+V",2)==0){
+            encstr = argv[i]+2; //key argument
+            add=1;
+        }
+        if(strncmp(argv[i]+2,"-V",2)==0){
+            encstr = argv[i]+2;
+            subtract=1;
+        }
+    }
+}
+
+    
+    int index=-1; //indicate where are we in the enc string
     while(feof(input)==0){//we have more input
         int c = fgetc(input);
-        c = encode(c);
+        index++;
+        if(c=='\0'){//cyclic
+            index=0;
+        }
+        c = encode(c,index);
         if(c!=EOF){ //handle last garbage char
             fputc(c,output);
         }
